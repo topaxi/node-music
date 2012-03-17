ab809a21ab8417e9fb8e85157c68a08d51e879c3
Damian Senn <damian.senn@gmail.com>
2012-03-17 13:34:33 +0100

    add script to cleanup dead artists/albums

---
+++ cleanup.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var async  = require('async')
+  , model  = require('./lib/model')
+  , Artist = model.Artist
+  , Album  = model.Album
+  , Track  = model.Track
+
+Artist.find({}, function(err, artists) {
+  async.forEach(artists, function(artist, done) {
+    Track.findOne({'artists': artist}, function(err, track) {
+      if (!track) {
+        console.log('removing', artist)
+
+        artist.remove()
+
+        done()
+      }
+    })
+  })
+})
+
+Album.find({}, function(err, albums) {
+  async.forEach(albums, function(album, done) {
+    Track.findOne({'album': album}, function(err, track) {
+      if (!track) {
+        console.log('removing', album)
+
+        album.remove()
+
+        done()
+      }
+    })
+  })
+})
